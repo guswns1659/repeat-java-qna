acdfef91598b315cb2a2f59b8478e2dcd08ff3a4
guswns1659 <guswns1659@gmail.com>
2020-03-21 18:34:26 +0900

    20.03.21) 질문하기 기능 구현
    - QuestionController 생성

--- src/main/java/com/codessquad/qna/HomeController.java
+++ src/main/java/com/codessquad/qna/HomeController.java
@@ -1,12 +1,15 @@
 package com.codessquad.qna;
 
+import com.codessquad.qna.domain.QuestionController;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("questions", QuestionController.questions);
         return "index";
     }
 }
--- src/main/java/com/codessquad/qna/domain/QuestionController.java
+++ src/main/java/com/codessquad/qna/domain/QuestionController.java
@@ -1,15 +1,31 @@
 package com.codessquad.qna.domain;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/questions")
 public class QuestionController {
+    public static List<Question> questions = new ArrayList<>();
+    private Logger logger = LoggerFactory.getLogger(QuestionController.class);
 
     @GetMapping("/form")
     public String form() {
         return "question/form";
+    }
+
+    @PostMapping("/create")
+    public String create(Question question) {
+        logger.info("question : {} ", question);
+        question.setId((long) questions.size() + 1);
+        questions.add(question);
+        return "redirect:/";
     }
 }
